a7adc6875c2c95943e4463db243ba6599dd50404
Steve Howell <steve@strings.com>
2012-09-23 12:14:00 -0700

    extracted header

--- json.c
+++ json.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "json.h"
 
-#define MAX_STRING_SIZE 128
-#define MAX_NUM_STRINGS 50
-#define MAX_NUM_ARRAYS 50
-#define MAX_ARRAY_SIZE 15
-
-#define TYPE_STRING 0
-#define TYPE_ARRAY 1
-
-struct element {
-  int type;
-  union {
-    char *s;
-    struct array *array;
-  } u;
-};
-
-struct array {
-  struct element *elements;
-  int i;
-};
-
-
-struct result {
-  int num_tokens;
-  int allocations;
-  char *strings[MAX_NUM_STRINGS+1];
-  int i_string;
-  struct array *arrays[MAX_NUM_ARRAYS+1];
-  int i_array;
-  struct element element;
-};
+void debug_array(struct array *array);
 
 void debug_element(struct element element) {
   if (element.type == TYPE_STRING) {
---
+++ json.h
@@ -0,0 +1,32 @@
+#define MAX_STRING_SIZE 128
+#define MAX_NUM_STRINGS 50
+#define MAX_NUM_ARRAYS 50
+#define MAX_ARRAY_SIZE 15
+
+#define TYPE_STRING 0
+#define TYPE_ARRAY 1
+
+struct element {
+  int type;
+  union {
+    char *s;
+    struct array *array;
+  } u;
+};
+
+struct array {
+  struct element *elements;
+  int i;
+};
+
+
+struct result {
+  int num_tokens;
+  int allocations;
+  char *strings[MAX_NUM_STRINGS+1];
+  int i_string;
+  struct array *arrays[MAX_NUM_ARRAYS+1];
+  int i_array;
+  struct element element;
+};
+
